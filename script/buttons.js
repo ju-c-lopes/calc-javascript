91946e0c73aca147cdfe65070d9a6ea7f76372dc
Juliano <juliano.co.lopes@gmail.com>
2022-03-19 10:05:01 -0300

    Show just one operator on visor

--- script/buttons.js
+++ script/buttons.js
@@ -1,28 +1,78 @@
+import soma from "./soma.js";
+
 const bts = document.querySelectorAll(".button");
 const visor = document.querySelector('#visor__result');
 const operations = ["+", "-", "*", "/", "=", ".", "C"];
+let operacao = null;
+let valor1 = null;
+let valor2 = null;
 
 for (let i = 0; i < bts.length; i++) {
     bts[i].addEventListener('click', () => {
         let selector = bts[i].innerText;
-
+        // variavel verify retorna se algum dos operadores matemáticos já foi clicado
+        let verify = 
+            visor.innerText.includes("+") ||
+            visor.innerText.includes("-") ||
+            visor.innerText.includes("*") ||
+            visor.innerText.includes("/");
         if (operations.includes(selector)) {
 
             switch(selector) {
                 case "+":
-                    console.log('mais');
+                    if (!verify) {
+                        visor.innerText += "+";
+                    }
+                    else {
+                        visor.innerText = visor.innerText.slice(0, visor.innerText.length - 1) + "+"
+                    }
                     break;
                 case "-":
+                    if (!verify) {
+                        visor.innerText += "-";
+                    }
+                    else {
+                        visor.innerText = visor.innerText.slice(0, visor.innerText.length - 1) + "-"
+                    }
                     break;
                 case "*":
+                    if (!verify) {
+                        visor.innerText += "*";
+                    }
+                    else {
+                        visor.innerText = visor.innerText.slice(0, visor.innerText.length - 1) + "*"
+                    }
                     break;
                 case "/":
+                    if (!verify) {
+                        visor.innerText += "/";
+                    }
+                    else {
+                        visor.innerText = visor.innerText.slice(0, visor.innerText.length - 1) + "/"
+                    }
                     break;
                 case "=":
+                    if (!valor1) return;
+                    else {
+                        valor2 = visor.innerText;
+                        switch(operacao) {
+                            case "+":
+                                break;
+                            case "-":
+                                break;
+                            case "*":
+                                break;
+                            case "/":
+                                break;
+                            default:
+                                return;
+                        }
+                    }
                     break;
                 case ".":
                     break;
                 case "C":
+                    // Limpa o visor
                     visor.innerText = 0;
                     break;
                 default:
@@ -30,7 +80,15 @@ for (let i = 0; i < bts.length; i++) {
             }
         }
         else {
-            if (visor.innerText != 0) {
+            if (verify) {
+                
+                if (!valor1) {
+                    operacao = visor.innerText.slice(visor.innerText.length - 1, visor.innerText.length);
+                    valor1 = visor.innerText.slice(0, visor.innerText.length - 1)
+                    visor.innerText = selector;
+                }
+            }
+            else if (visor.innerText != 0) {
                 visor.innerText += selector;
             }
             else {
@@ -40,7 +98,4 @@ for (let i = 0; i < bts.length; i++) {
     })
 }
 
-//() => {
-//    window.addEventListener('click', () => { 
-//    console.log(document.querySelector('.button--1').innerText);
-//}
+export default { visor };
---
+++ script/soma.js
@@ -0,0 +1,5 @@
+function soma(valor1) {
+  return valor1 + "feito";
+}
+
+export default soma;
